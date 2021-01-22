71309a7b91dec1d8ebca01fc720f89e76a01ce5b
Pete Wilcock <pete.wilcock@gmail.com>
2021-01-22 22:43:25 +0000

    Cleanup and some additional comment

--- main.tf.tf
+++ main.tf.tf
@@ -1,10 +1,10 @@
-resource "null_resource" "wordpress_pullpush" {
+resource "null_resource" "docker_pullpush" {
 
   triggers = {
     shell_hash = sha256(var.docker_source)
   }
   provisioner "local-exec" {
-      // ARGs to script are source image, region, AWS Account ID, aws_profile
-      command = "${abspath(path.module)}/docker_pullpush.sh ${var.docker_source} ${var.aws_region} ${var.aws_account_id} ${var.aws_profile}"
+      // ARGs to script are source image, region, AWS Account ID, aws_profile, ECR Repo, Image Tag
+      command = "${abspath(path.module)}/docker_pullpush.sh ${var.docker_source} ${var.aws_region} ${var.aws_account_id} ${var.aws_profile} ${var.ecr_repo_name} ${var.ecr_repo_tag}"
   }
 }
