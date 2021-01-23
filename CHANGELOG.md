f67d2cffbdbf3f2467b4ce82831b9ee413aeb349
Pete Wilcock <pete.wilcock@gmail.com>
2021-01-23 14:04:22 +0000

    Fixing region bias

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.0.5
+
+Remove region bias for push
+
 ## 0.0.4
 
 Bugfix on executability of script
--- docker_pullpush.sh
+++ docker_pullpush.sh
@@ -6,5 +6,5 @@ fi
 
 docker pull $1
 aws ecr get-login-password --region $2 $profile | docker login --username AWS --password-stdin $3.dkr.ecr.$2.amazonaws.com
-docker tag $1 $3.dkr.ecr.eu-west-1.amazonaws.com/$4:$5
-docker push $3.dkr.ecr.eu-west-1.amazonaws.com/$4:$5
+docker tag $1 $3.dkr.ecr.$2.amazonaws.com/$4:$5
+docker push $3.dkr.ecr.$2.amazonaws.com/$4:$5
