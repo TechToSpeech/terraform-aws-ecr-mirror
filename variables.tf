fc0b86fe15e273a5f1d7cf225c90184bc5db56b8
Pete Wilcock <petewilcock@users.noreply.github.com>
2021-06-07 22:30:46 +0100

    Handling Windows paths (#1)

--- variables.tf
+++ variables.tf
@@ -1,24 +1,24 @@
 variable "aws_region" {
-    description = "The region in which the ECR repository is located."
+  description = "The region in which the ECR repository is located."
 }
 
 variable "docker_source" {
-    description = "The source location of the Docker image being pulled."
+  description = "The source location of the Docker image being pulled."
 }
 
 variable "aws_account_id" {
-    description = "The AWS Account ID where the ECR repository is located."
+  description = "The AWS Account ID where the ECR repository is located."
 }
 
 variable "aws_profile" {
-    description = "The awscli profile name (located in the ~/.aws/credentials file) used to authenticate the ECR login and push (Optional)"
-    default = ""
+  description = "The awscli profile name (located in the ~/.aws/credentials file) used to authenticate the ECR login and push (Optional)"
+  default     = ""
 }
 
 variable "ecr_repo_name" {
-    description = "The name of the ECR repository being pushed to."
+  description = "The name of the ECR repository being pushed to."
 }
 
 variable "ecr_repo_tag" {
-    description = "The tag of the ECR repository image being pushed."
+  description = "The tag of the ECR repository image being pushed."
 }
