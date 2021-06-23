529085b7dc26bf242bfc622584210988852386d4
jvillamonte <80098479+jvillamonte@users.noreply.github.com>
2021-06-23 13:30:03 +0100

    Adding bash as command interpreter (#2)

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ resource "null_resource" "docker_pullpush" {
     shell_hash = sha256("${var.docker_source}${var.ecr_repo_name}${var.ecr_repo_tag}")
   }
   provisioner "local-exec" {
+    interpreter = ["bash", "-c"]
     command = "${local.module_path}/docker_pullpush.sh ${var.docker_source} ${var.aws_region} ${var.aws_account_id} ${var.ecr_repo_name} ${var.ecr_repo_tag} ${var.aws_profile}"
   }
 }
