9b6b6293126aa54929c9d84522a3a8ca9ddf0895
Pete Wilcock <pete.wilcock@gmail.com>
2022-05-15 09:34:12 +0100

    Update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "ecr_repo_arn" {
+output "ecr_repo_url" {
+    value = "${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${var.ecr_repo_name}"
+    description = "The repository URL of the pushed image."
+}
+
+output "ecr_repo_url_tag" {
     value = "${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${var.ecr_repo_name}:${var.ecr_repo_tag}"
     description = "The repository URL with tag of the pushed image."
 }
